b1ffd214e5f24b61462ef7e577bc61fa5910b573
aditya-2k23 <holaaditya123@gmail.com>
2025-12-10 00:04:28 +0530

    Enhance image preprocessing function to include autocontrast and cleanup steps

--- utils.py
+++ utils.py
@@ -1,11 +1,29 @@
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
+import io
+import base64
 
 def preprocess_image(file):
+    # Load
     img = Image.open(file).convert("L")
-    img = img.resize((28, 28))
-    img_array = np.array(img)
-    img_array = 255 - img_array
-    img_array = img_array / 255.0
-    img_array = img_array.reshape(1, 28, 28, 1)
-    return img_array
+
+    # Autocontrast + cleanup
+    img = ImageOps.invert(img)
+    img = ImageOps.autocontrast(img)
+    img = ImageOps.invert(img)
+
+    # Resize
+    img = img.resize((28, 28), Image.Resampling.LANCZOS)
+
+    # For model
+    arr = np.array(img)
+    arr = 255 - arr
+    arr = arr / 255.0
+    arr = arr.reshape(1, 28, 28, 1)
+
+    # Convert processed 28×28 image to base64
+    buffered = io.BytesIO()
+    img.save(buffered, format="PNG")
+    img_bytes = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+    return arr, img_bytes
